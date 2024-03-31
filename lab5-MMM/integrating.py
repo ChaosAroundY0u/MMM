e3dae9bc4a8b6b4c56abef9ff96826a875817a5a
ChaosAroundY0u <152779643+ChaosAroundY0u@users.noreply.github.com>
2024-03-31 12:50:34 +0300

    fully updated integrating.py

--- lab5-MMM/integrating.py
+++ lab5-MMM/integrating.py
@@ -2,40 +2,32 @@ import numpy as np
 eps = 10**(-4)
 a = 0
 b = 9
-h = (b - a) / 10000
+start = 10
 
 def func(x):
     return (np.sin(x) * np.log(1 + x)) / (x*x * np.sqrt(x))
 
-ans = []
-x0 = a + h / 2
-y0 = func(x0)
-ans.append(y0)
-for i in range(10000-1):
-    xi = x0 + h
-    yi = func(xi)
-    ans.append(yi)
-    x0 = xi
-print(round(h*sum(ans), 5))
+def calculating(start):
+    ans = []
+    h = (b - a) / start
+    x0 = a + h / 2
+    y0 = func(x0)
+    ans.append(y0)
+    for i in range(start - 1):
+        xi = x0 + h
+        yi = func(xi)
+        ans.append(yi)
+        x0 = xi
+    return h*sum(ans)
+i = 1
+k = 0
+first_integral = 0
+second_integral = calculating(start)
+print("iteration ", k, ":", second_integral, "||", "pogr = ", second_integral - first_integral)
 
-# import numpy as np
-# eps = 10**(-4)
-# a = 2
-# b = 5
-# h = (b - a) / 5
-
-# def func(x):
-#     return (1 / np.log(x))
-
-# ans = []
-# x0 = a + h / 2
-# y0 = func(x0)
-# ans.append(y0)
-# print(x0)
-# for i in range(5-1):
-#     xi = x0 + h
-#     yi = func(xi)
-#     print(xi)
-#     ans.append(yi)
-#     x0 = xi
-# print(h*sum(ans))
+while abs(second_integral - first_integral) > eps:
+    first_integral = second_integral
+    i *= 2
+    second_integral = calculating(start * i)
+    k += 1
+    print("iteration ", k, ":", second_integral, "||", "pogr = ", second_integral - first_integral)
