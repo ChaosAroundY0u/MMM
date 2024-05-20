405d2ba8eaaf872ee5299f3c3b8e9cce98e48ed3
ChaosAroundY0u <152779643+ChaosAroundY0u@users.noreply.github.com>
2024-05-20 19:16:21 +0300

    it's over 2.0

--- lab8-MMM/Adams_main_quest.py
+++ lab8-MMM/Adams_main_quest.py
@@ -5,20 +5,29 @@ from numpy.linalg import norm
 def f(x):
     return np.sin(4*x) * np.exp(x)
 
-def adams(N):
+def adams(f, N):
     a, b = 0, 2
-    h = 2 / N
-    x = np.arange(a, b, h)
+    h = (b - a) / N
+    x = np.arange(a, b + h, h)
     y = np.zeros(N+1)
     y[0] = 1
     y1 = np.zeros(N+1)
     y1[0] = 0
     
-    y2 = f(x[0]) - 16*y1[0] - 16*y[0]
-    for i in range(1, N):
-        y[i+1] = y[i] + h*y1[i]
-        y1[i+1] = y1[i] + h*y2
-        y2 = f(x[i]) - 16*y1[i] - 16*y[i]
+    for i in range(1, 2):
+        k1 = h * y1[i-1]
+        l1 = h * (f(x[i-1]) - 16*y1[i-1] + 16*y[i-1])
+        k2 = h * (y1[i-1] + l1)
+        l2 = h * (f(x[i-1]+h) - 16*(y1[i-1]+k1) + 16*(y[i-1]+k2))
+        y[i] = y[i-1] + (k1 + k2) / 2
+        y1[i] = y1[i-1] + (l1 + l2) / 2
+
+    for i in range(2, N+1):
+        y_pred = y[i-1] + h * (1.5 * y1[i-1] - 0.5 * y1[i-2])
+        y1_pred = y1[i-1] + h * (1.5 * (f(x[i-1]) - 16*y1[i-1] + 16*y[i-1]) - 0.5 * (f(x[i-2]) - 16*y1[i-2] + 16*y[i-2]))
+        y[i] = y[i-1] + h * (1.5 * y1_pred - 0.5 * y1[i-1])  
+        y1[i] = y1[i-1] + h * (1.5 * (f(x[i-1]) - 16*y1_pred + 16*y_pred) - 0.5 * (f(x[i-2]) - 16*y1[i-2] + 16*y[i-2]))
+
     return [x, y, y1, h]
 
 
@@ -27,10 +36,10 @@ eps = 0.01
 count = 0
 while True:
     count += 1
-    A = adams(N)
-    A2 = adams(2*N)
+    A = adams(f, N)
+    A2 = adams(f, 2*N)
     err = (norm(A[1] - A2[1][0::2]) + norm(A[2] - A2[2][0::2]))/(norm(A2[1]) + norm(A2[2]))
-    N *= 2
+    N *=2
     if err < eps:
         print("Погрешность:", err)
         break
@@ -41,7 +50,7 @@ print("Итераций:",count)
 print("Шаг:", A2[3])
 #y(x)
 plt.figure(figsize=(10,4))
-plt.plot(X,Y, label = "Численное")
+plt.plot(X[1:], Y, label = "Численное")
 plt.grid(True)
 plt.xlabel('x')
 plt.ylabel('y')
@@ -52,7 +61,7 @@ plt.show()
 #y'(x)
 plt.figure(figsize=(10,4))
 plt.subplot(1,2,1)
-plt.plot(X,Y1, label = "Численное")
+plt.plot(X[1:] ,Y1, label = "Численное")
 plt.grid(True)
 plt.xlabel('x')
 plt.ylabel("y'")
